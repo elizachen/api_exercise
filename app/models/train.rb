83c5dd9ddf6301d88b057b13de9548655a35a17f
eliza chen <elizachen34@gmail.com>
2017-03-24 11:30:55 +0800

    API routes, api controller modification

---
+++ app/models/train.rb
@@ -0,0 +1,9 @@
+class Train < ApplicationRecord
+  validates_presence_of :number
+  has_many :reservations
+
+  def availabel_seats
+    # TODO :  trick first
+    ["1A", "1B", "1C", "1D", "1F"]
+  end
+end
