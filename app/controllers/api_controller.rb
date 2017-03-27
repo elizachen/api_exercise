29079ea02023ccbc579a9fba5c517ead3115154c
eliza chen <elizachen34@gmail.com>
2017-03-27 16:52:51 +0800

    Implement AuthController

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -3,12 +3,12 @@ class ApiController < ActionController::Base
 
   def authenticate_user_from_token!
 
-    if params[:auth_token].presesnt?
+    if params[:auth_token].present?
       user = User.find_by_authentication_token( params[:auth_token] )
 
       # sign_in 是 Devise 的方法，会设定好 current_user
       sign_in(user, store: false) if user
-      
+
     end
   end
 
